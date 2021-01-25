26439027052b445582fa56189ebf0e1942a5975e
Ilya-Tatarchenko <tatarchenkoii2003@gmail.com>
2021-01-25 09:22:13 +0300

    release2

--- game.js
+++ game.js
@@ -1,6 +1,18 @@
 var defenceCounter = 0;
 var enrageCounter = 0;
 
+//информация о здоровье
+let hunterHealth = document.querySelector('#hunter_health');
+let monsterHealth = document.querySelector('#monster_health');
+
+//кнопки атаки и спец. умений
+let hunterAttack = document.querySelector('#hunter_attack');
+let monsterAttack = document.querySelector('#monster_attack');
+let block = document.querySelector('#hunter_defence');
+let heal = document.querySelector('#hunter_heal');
+let enrage = document.querySelector('#monster_enrage');
+let fury = document.querySelector('#monster_fury'); 
+
 class Fighter 
 {
     constructor(obj) 
@@ -41,16 +53,19 @@ class Fighter
         this.attack = value;
     }
 
+
     fight(enemy) 
     {
         if(enemy == hunter)
         {
+            //использование способности ЗАЩИТА
             if((defenceCounter != 0) & (enemy.hitpoints + 1 <= enemy.totalHitpoints))
             {
                 enemy.hitpoints = enemy.hitpoints + 1;
                 defenceCounter = 0;
             }
 
+            //использование способности ДВОЙНОЙ УРОН
             else if((enrageCounter > 0) & (enemy.hitpoints - (2 * this.attack) >= 0))
             {
                 enemy.hitpoints = enemy.hitpoints - (2 * this.attack);
@@ -59,10 +74,13 @@ class Fighter
 
             else
             {
-                if(enemy.hitpoints < 10)
+                if(enemy.hitpoints <= this.attack)
                 {
                     enemy.hitpoints = enemy.hitpoints - enemy.hitpoints;
-                    console.log('Hero have died');
+                    this.totalHitpoints = this.totalHitpoints + (0.1 * enemy.totalHitpoints);
+                    this.hitpoints = this.totalHitpoints;
+                    enemy.hitpoints = 0.25 * enemy.totalHitpoints; 
+                    alert('Monster have won');
                 }
                 else
                 {
@@ -71,12 +89,15 @@ class Fighter
             }
         }
 
-        else
+        else if(enemy == beast)
         {
             if(enemy.hitpoints <= 10)
             {
                 enemy.hitpoints = enemy.hitpoints - enemy.hitpoints;
-                console.log('Hero have died');
+                this.attack = this.attack + 1;
+                enemy.hitpoints = enemy.totalHitpoints;
+                alert('Monster have been passed');   
+                alert('+1 to your attack');
             }
             else
             {
@@ -99,6 +120,7 @@ class Champion extends Fighter
         super(obj);
     }
 
+    //текущее здоровье увеличивается на 5
     heal()
     {
         if(this.hitpoints + 5 <= this.totalHitpoints)
@@ -107,10 +129,12 @@ class Champion extends Fighter
         }
         else
         {
-            console.log('You can not heal now');
+            alert('You can not heal now');
         }
     }
 
+    //следующая атака монстра не нанесет урона, а
+    //текущее здоровье героя возрастет на 1
     defence()
     {
         defenceCounter ++;
@@ -125,11 +149,14 @@ class Monster extends Fighter
         super(obj);
     }
 
+    //следующие две атаки монстра наносят двойной урон
     enrage()
     {
         enrageCounter = enrageCounter + 2;
     }
 
+    //атака монстра возрастает на 2 еденицы, но 
+    //общее здоровье уменьшается на 5 едениц
     fury()
     {
         this.hitpoints = this.hitpoints - 5;
@@ -141,20 +168,36 @@ class Monster extends Fighter
 var hunter = new Champion({name: "Rexxar", attack: 10, hitpoints: 60});
 var beast = new Monster({name: "King Krush", attack: 8, hitpoints: 80});
 
-beast.fury();
-beast.fight(hunter);
-hunter.defence();
-beast.fight(hunter);
-beast.fight(hunter);
-hunter.fight(beast);
-hunter.heal();
-hunter.heal();
-beast.enrage();
-beast.fight(hunter);
-hunter.setHitponts(40);
-beast.fight(hunter);
-beast.fight(hunter);
-beast.fight(hunter);
+hunterAttack.addEventListener('click', function(event) {
+    hunter.fight(beast);
+    monsterHealth.innerHTML = beast.getHitpoints();
+});
 
-console.log(hunter.getHitpoints());
-console.log(beast.getHitpoints());
+monsterAttack.addEventListener('click', function(event) {
+    beast.fight(hunter);
+    hunterHealth.innerHTML = hunter.getHitpoints();
+});
+
+block.addEventListener('click', function(event) {
+    hunter.defence();
+    hunterHealth.innerHTML = hunter.getHitpoints();
+});
+
+heal.addEventListener('click', function(event) {
+    hunter.heal();
+    hunterHealth.innerHTML = hunter.getHitpoints();
+});
+
+enrage.addEventListener('click', function(event) {
+    beast.enrage();
+});
+
+fury.addEventListener('click', function(event) {
+    beast.fury();
+    monsterHealth.innerHTML = beast.getHitpoints();
+});
+
+hunterHealth.innerHTML = hunter.getHitpoints();
+monsterHealth.innerHTML = beast.getHitpoints();
+
+
